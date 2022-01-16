439a5b93542cafa623b197070b64b17d85e3de9a
yuliangyong <yuliangyong8295019@gmail.com>
2022-01-16 12:33:24 -0800

    integrating dht and blockchain

--- client/client/product.go
+++ client/client/product.go
@@ -5,3 +5,8 @@ type Product struct {
 	Owner string  // ip address of the owner
 	Stock int     // number of the product
 }
+
+type User struct {
+	Name string    // name of the user
+	Address string // ip address of the user
+}
