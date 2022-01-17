9fa49e0d069c7e9efd63eb77d4302010abc011cf
yuliangyong <yuliangyong8295019@gmail.com>
2022-01-16 16:01:32 -0800

    cli: two nodes receive reach other

--- cli/main.go
+++ cli/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"github.com/ethereum/go-ethereum/crypto"
 	z "go.dedis.ch/cs438/internal/testing"
 	"go.dedis.ch/cs438/registry/standard"
 	"go.dedis.ch/cs438/transport/udp"
+	"os"
+	"strings"
 	"time"
 )
 
 func main() {
+	// initialize
 	transp := udp.NewUDP()
 	bitNum := 12
-	socket, _ := transp.CreateSocket("127.0.0.1:8000")
+	socket, _ := transp.CreateSocket("127.0.0.1:0")
 	privateKey, _ := crypto.GenerateKey()
 	clientNode := *z.NewClient(nil,
 		z.WithSocket(socket),
@@ -22,9 +27,29 @@ func main() {
 		z.WithStabilizeInterval(time.Millisecond*500),
 		z.WithFixFingersInterval(time.Millisecond*250))
 	clientNode.Start()
-	clientNode.AddPeers("127.0.0.1:8000")
-	time.Sleep(time.Second * 5)
+	time.Sleep(time.Second * 2)
 
-	time.Sleep(time.Second * 1000)
+	// read from command
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Println("Waiting for input...")
+		paramsS, err := reader.ReadString('\n')
+		paramsS = strings.TrimRight(paramsS, "\n")
+		if err != nil {
+			fmt.Println(err)
+		}
+		params := strings.Split(paramsS, " ")
+		action := params[0]
+		if action == "AddPeer" {
+			addr := params[1]
+			fmt.Println(addr)
+			clientNode.AddPeers(addr)
+		}
+	}
+
+	//clientNode.AddPeers("127.0.0.1:0")
+	//time.Sleep(time.Second * 5)
+	//
+	//time.Sleep(time.Second * 1000)
 	clientNode.Stop()
 }
