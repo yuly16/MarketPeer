900d9653b15ded04faf557f0a6c26a2a9eb134cd
yuliangyong <yuliangyong8295019@gmail.com>
2022-01-16 20:18:03 -0800

    change cli

--- cli/main.go
+++ cli/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
@@ -136,38 +135,61 @@ func main() {
 	fmt.Println(clientNode.BlockChainFullNode.GetChain().String())
 	time.Sleep(time.Second * 2)
 
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Println("Type Init to init chord, type Join to join chord:")
-		params, _ := reader.ReadString('\n')
-		params = strings.TrimRight(params, "\n")
-		if params == "Init" {
-			if c.Nodes[0].Addr == addr {
-				clientNode.ChordNode.Init(c.Nodes[1].Addr)
-				break
-			} else if c.Nodes[1].Addr == addr {
-				clientNode.ChordNode.Init(c.Nodes[0].Addr)
-				break
-			} else {
-				fmt.Println("This node should input Join.")
-			}
-		} else if params == "Join" {
-			if c.Nodes[0].Addr == addr {
-				fmt.Println("This node should input Init.")
-			} else if c.Nodes[1].Addr == addr {
-				fmt.Println("This node should input Init.")
-			} else {
-				err := clientNode.ChordNode.Join(c.Nodes[0].Addr)
-				if err != nil {
-					return
-				}
-				break
-			}
-		} else {
-			fmt.Println("wrong format. ")
+	//reader := bufio.NewReader(os.Stdin)
+	//for {
+	//	fmt.Println("Type Init to init chord, type Join to join chord:")
+	//	params, _ := reader.ReadString('\n')
+	//	params = strings.TrimRight(params, "\n")
+	//	if params == "Init" {
+	//		if c.Nodes[0].Addr == addr {
+	//			clientNode.ChordNode.Init(c.Nodes[1].Addr)
+	//			break
+	//		} else if c.Nodes[1].Addr == addr {
+	//			clientNode.ChordNode.Init(c.Nodes[0].Addr)
+	//			break
+	//		} else {
+	//			fmt.Println("This node should input Join.")
+	//		}
+	//	} else if params == "Join" {
+	//		if c.Nodes[0].Addr == addr {
+	//			fmt.Println("This node should input Init.")
+	//		} else if c.Nodes[1].Addr == addr {
+	//			fmt.Println("This node should input Init.")
+	//		} else {
+	//			err := clientNode.ChordNode.Join(c.Nodes[0].Addr)
+	//			if err != nil {
+	//				return
+	//			}
+	//			break
+	//		}
+	//	} else {
+	//		fmt.Println("wrong format. ")
+	//	}
+	//}
+	var chordOp string
+	if c.Nodes[0].Addr == addr || c.Nodes[1].Addr == addr {
+		chordOp = "Init"
+	} else {
+		chordOp = "Join"
+	}
+	chord_prompt := promptui.Select{
+		Label: "Choose to start chord",
+		Items: []string{chordOp},
+	}
+	_, chord_op, _ := chord_prompt.Run()
+	switch chord_op {
+	case "Init":
+		if c.Nodes[0].Addr == addr {
+			clientNode.ChordNode.Init(c.Nodes[1].Addr)
+		} else if c.Nodes[1].Addr == addr {
+			clientNode.ChordNode.Init(c.Nodes[0].Addr)
+		}
+	case "Join":
+		err := clientNode.ChordNode.Join(c.Nodes[0].Addr)
+		if err != nil {
+			return
 		}
 	}
-
 	// read from command
 	// reader := bufio.NewReader(os.Stdin)
 	// for {
@@ -188,38 +210,39 @@ func main() {
 
 	// Claim validation function for each command
 	addpeer_validate := func(input string) error {
-		fmt.Println("Here is your input command", 1+1)
 		return nil
 	}
 	// Claim validation function for each command
 	initchord_validate := func(input string) error {
-		fmt.Println("Here is your input command", 1+1)
 		return nil
 	}
 	// Claim validation function for each command
 	joinchord_validate := func(input string) error {
-		fmt.Println("Here is your input command", 1+1)
 		return nil
 	}
 	inputproduct_validate := func(input string) error {
-		fmt.Println("Here is your input command", 1+1)
-		return nil
+		params := strings.Split(input, " ")
+		if len(params) == 2 {
+			return nil
+		} else {
+			return fmt.Errorf("error")
+		}
 	}
 	viewproduct_validate := func(input string) error {
-		fmt.Println("Here is your input command", 1+1)
-		return nil
+		params := strings.Split(input, " ")
+		if len(params) == 1 {
+			return nil
+		} else {
+			return fmt.Errorf("error")
+		}
 	}
 	transfer_validate := func(input string) error {
-		fmt.Println("Here is your input command", 1+1)
-		return nil
-	}
-	showaccount_validate := func(input string) error {
-		fmt.Println("Here is your input command", 1+1)
-		return nil
-	}
-	showchain_validate := func(input string) error {
-		fmt.Println("Here is your input command", 1+1)
-		return nil
+		params := strings.Split(input, " ")
+		if len(params) == 2 {
+			return nil
+		} else {
+			return fmt.Errorf("error")
+		}
 	}
 	// Front-end CLI UI
 	for {
@@ -259,14 +282,6 @@ func main() {
 		transfer_prompt := promptui.Prompt{
 			Label:	"[Transfer] input transfer (format: Value To): ",
 			Validate: transfer_validate,
-		}
-		showaccount_prompt := promptui.Prompt{
-			Label:	"[Show Account]",
-			Validate: showaccount_validate,
-		}
-		showchain_prompt := promptui.Prompt{
-			Label:	"[Show Chain]",
-			Validate: showchain_validate,
 		}
 		switch cmd {
 		case "Add Peer":
@@ -338,7 +353,6 @@ func main() {
 			params := strings.Split(info, " ")
 			value, _ := strconv.Atoi(params[0])
 			to := params[1]
-			fmt.Println(value, to)
 			addrBytesSlice, _ := hex.DecodeString(to)
 			addrBytes := [8]byte{}
 			copy(addrBytes[:], addrBytesSlice)
@@ -349,11 +363,9 @@ func main() {
 				fmt.Println(clientNode.BlockChainFullNode.ShowAccount().String())
 			}
 		case "ShowAccount":
-			_, _ = showaccount_prompt.Run()
 			info := clientNode.BlockChainFullNode.ShowAccount()
 			fmt.Println("account info: " + info.String())
 		case "ShowChain":
-			_, _ = showchain_prompt.Run()
 			fmt.Println(clientNode.BlockChainFullNode.GetChain().String())
 			// execution
 		case "Exit":
