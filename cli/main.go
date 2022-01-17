ae6e6914dc4985e8f78b2385aeb709c62cfa555e
Lin Yuan <lin.yuan@epfl.ch>
2022-01-17 02:16:18 +0100

    Update main.go

--- cli/main.go
+++ cli/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"github.com/ethereum/go-ethereum/crypto"
 	z "go.dedis.ch/cs438/internal/testing"
 	"go.dedis.ch/cs438/registry/standard"
 	"go.dedis.ch/cs438/transport/udp"
-	"os"
-	"strings"
+	// "os"
+	// "strings"
 	"time"
+
+	"github.com/manifoldco/promptui"
 )
 
 func main() {
@@ -31,20 +32,63 @@ func main() {
 	time.Sleep(time.Second * 2)
 
 	// read from command
-	reader := bufio.NewReader(os.Stdin)
+	// reader := bufio.NewReader(os.Stdin)
+	// for {
+	// 	fmt.Println("Waiting for input...")
+	// 	paramsS, err := reader.ReadString('\n')
+	// 	paramsS = strings.TrimRight(paramsS, "\n")
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// 	params := strings.Split(paramsS, " ")
+	// 	action := params[0]
+	// 	if action == "AddPeer" {
+	// 		addr := params[1]
+	// 		fmt.Println(addr)
+	// 		clientNode.AddPeers(addr)
+	// 	}
+	// }
+	
+	// Claim validation function for each command
+	addpeer_validate := func(input string) error {
+		fmt.Println("Here is your input command", 1+1)
+		return nil
+	}
+
+	// Front-end CLI UI
 	for {
-		fmt.Printf("> ")
-		paramsS, err := reader.ReadString('\n')
-		paramsS = strings.TrimRight(paramsS, "\n")
-		if err != nil {
-			fmt.Println(err)
+		cmd_prompt := promptui.Select{
+			Label: "Select your command:",
+			Items: []string{"Add Peer", "View Products", "Input Product Information", "Exit"},
 		}
-		params := strings.Split(paramsS, " ")
-		action := params[0]
-		if action == "AddPeer" {
-			addr := params[1]
-			fmt.Println(addr)
-			clientNode.AddPeers(addr)
+
+		is_exit := false
+		_, cmd, err := cmd_prompt.Run()
+
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+			return
+		}
+
+		addpeer_prompt := promptui.Prompt{
+			Label:	"[Add peer] input a valid IP address: ",
+			Validate: addpeer_validate,
+		}
+
+		switch cmd {
+		case "Add Peer":
+			_, err := addpeer_prompt.Run()
+			if err != nil {
+				fmt.Printf("Prompt failed %v\n", err)
+				return
+			}
+			// execution
+		case "Exit":
+			is_exit = true
+		}
+
+		if is_exit {
+			break
 		}
 	}
 
